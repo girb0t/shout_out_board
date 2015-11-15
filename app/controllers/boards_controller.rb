6fff4fcc962a5ed0fe20a77c27388c04a26b1528
Pei Xiong Liu <peixiong.liu@gmail.com>
2015-11-15 12:46:08 -0500

    Add board create route/controller method

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -7,4 +7,19 @@ class BoardsController < ApplicationController
     is_valid = Board.is_unique_key?(key)
     render json: {isValid: is_valid, key: key}
   end
+
+  def create
+    form_data = JSON.parse(params["form-data-json"])
+    board = Board.new( key: form_data['key']['value'],
+                               title: form_data['title']['title'])
+
+    (1..form_data["categoryCount"]).each do |i|
+      category_hash = form_data["category" + i.to_s]
+      category = Category.new(title: category_hash["title"], prompt: category_hash["prompt"])
+
+      board.categories.push(category)
+    end
+
+    board.save
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   # boards
   get  '/boards/new',             to: 'boards#new'
   # get  '/boards/:key',            to: 'boards#show'
-  # post '/boards',                 to: 'boards#create'
+  post '/boards',                 to: 'boards#create'
   get  '/boards/validate_key',    to: 'boards#validate_key'
 end
