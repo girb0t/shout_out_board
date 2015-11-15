42264fe7f99c61fc564a1b0ca1305e0809ba1d34
Pei Xiong Liu <peixiong.liu@gmail.com>
2015-11-15 12:42:28 -0500

    Add board key validation controller method

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,4 +1,10 @@
 class BoardsController < ApplicationController
   def new
   end
+
+  def validate_key
+    key = params[:key]
+    is_valid = Board.is_unique_key?(key)
+    render json: {isValid: is_valid, key: key}
+  end
 end
