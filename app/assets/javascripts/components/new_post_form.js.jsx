8c5bb8e060ca612a2f19423cf4be741f43aa001d
Pei Xiong Liu <peixiong.liu@gmail.com>
2015-11-23 17:50:14 -0500

    Add NewPostForm react component

---
+++ app/assets/javascripts/components/new_post_form.js.jsx
@@ -0,0 +1,12 @@
+var NewPostForm = NewPostForm || {};
+
+(function() {
+  "use strict";
+  NewPostForm = React.createClass({
+    render: function() {
+      return (
+        <div>New Post Form React</div>
+      );
+    }
+  });
+})();
