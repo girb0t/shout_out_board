f17d3ca6a0f461b9fdd92c3408054f1951c752f3
Pei Xiong Liu <peixiong.liu@gmail.com>
2015-11-26 23:09:16 -0500

    sort posts by created_at asc order

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ActiveRecord::Base
   belongs_to :board
-  has_many :posts
+  has_many :posts, -> { order(created_at: :asc) }
   validates :title, presence: true
   validates :prompt, presence: true
 end
