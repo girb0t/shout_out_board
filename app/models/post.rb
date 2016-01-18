85d1de735a1db5f495bae28807fdb2b90af6b4d0
Pei Xiong Liu <peixiong.liu@gmail.com>
2016-01-18 18:58:43 -0500

    Update Post model to allow blank for hex colors

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ActiveRecord::Base
   belongs_to :category
   validates :body, presence: true
   validates :background_color_hex, format: { with: /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i,
-    message: "only allows hex notation"}
+    message: "only allows hex notation"}, allow_blank: true
   validates :font_color_hex, format: { with: /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i,
-    message: "only allows hex notation"}
+    message: "only allows hex notation"}, allow_blank: true
 end
