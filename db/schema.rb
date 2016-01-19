d8d1d22eca3ef653c92e36113b75d7fa62e672c9
Pei Xiong Liu <peixiong.liu@gmail.com>
2016-01-18 19:17:03 -0500

    create_users migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160107102856) do
+ActiveRecord::Schema.define(version: 20160119000326) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,14 @@ ActiveRecord::Schema.define(version: 20160107102856) do
     t.datetime "updated_at"
     t.string   "background_color_hex", limit: 7
     t.string   "font_color_hex",       limit: 7
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",           null: false
+    t.string   "username",        null: false
+    t.string   "password_digest", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
