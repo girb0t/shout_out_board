c70e7c7876a89916bae9ee29313226c9082ba6c7
Pei Xiong Liu <peixiong.liu@gmail.com>
2016-06-11 20:05:33 -0400

    REMOVE EMAIL FROM USER

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,6 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :boards
   has_many :posts
-  validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i,
-                      message: "only allows email format" },
-                    presence: true,
-                    uniqueness: true
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :username, presence: true, uniqueness: true
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160611094737) do
+ActiveRecord::Schema.define(version: 20160611195405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,7 +49,6 @@ ActiveRecord::Schema.define(version: 20160611094737) do
   add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",           null: false
     t.string   "username",        null: false
     t.string   "password_digest", null: false
     t.datetime "created_at"
