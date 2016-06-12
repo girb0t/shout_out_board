3934fb03ae27d0250cf517d1f99222841aa8ed5c
Pei Xiong Liu <peixiong.liu@gmail.com>
2016-06-12 19:12:06 -0400

    Write user_spec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe User do
+  it "has a valid factory" do
+    expect(build(:user)).to be_valid
+  end
+
+  let(:user) { build(:user) }
+
+  describe "validations" do
+    it { expect(user).to validate_presence_of(:first_name) }
+    it { expect(user).to validate_presence_of(:last_name) }
+    it { expect(user).to validate_presence_of(:username) }
+  end
+end
+
