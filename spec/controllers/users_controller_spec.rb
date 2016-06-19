986b7084fdf5e82f51cb6b617246de8db6a330c9
Pei Xiong Liu <peixiong.liu@gmail.com>
2016-06-18 23:28:10 -0400

    Add user controller tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe UsersController do
   let(:user) { create :user }
 
-  describe "GET new" do
+  describe "GET #new" do
     context "user is logged in" do
       before(:each) do
         session[:user_id] = user.id
@@ -14,5 +14,44 @@ describe UsersController do
         expect(response).to redirect_to root_path
       end
     end
+
+    context "user is NOT logged in" do
+      it "renders the new template" do
+        get :new
+        expect(response).to render_template :new
+      end
+    end
+  end
+
+  describe "POST #create" do
+    context "with valid attributes" do
+      it "creates a new user in the database" do
+        expect {
+          post :create, 'form-data-json' => { firstName: 'Luke',
+                                              lastName: 'Skywalker',
+                                              username: 'yoda42',
+                                              password: 'password',
+                                              password_confirmation: 'password' }.to_json
+        }.to change(User, :count)
+      end
+
+      it "saves the user id to the session" do
+      end
+
+      it "redirects to boards_new_path" do
+      end
+
+      it "saves message to flash success" do
+      end
+    end
+
+    context "with invalid attributes" do
+      it "redirects to signup_path" do
+      end
+
+      it "saves message to flash danger" do
+      end
+    end
+
   end
 end
