e7f07764db55160d049470774aa236f5acd3c534
Pei Xiong Liu <peixiong.liu@gmail.com>
2016-06-19 12:15:18 -0400

    Reset session upon authentication to protect against session fixation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,23 +1,30 @@
 class SessionsController < ApplicationController
   def new
+    redirect_to boards_path if logged_in?
+    # otherwise render 'new.html.erb'
   end
 
   def create
+    #protect against session fixation: http://guides.rubyonrails.org/security.html#session-fixation
+    temp_session = session.dup
+    reset_session
+    session.replace(temp_session)
+
     user = User.find_by(username: params[:username])
 
     if user && user.authenticate(params[:password])
       flash[:success] = "Welcome back #{user.first_name} :)"
       session[:user_id] =  user.id
-      redirect_to '/'
+      redirect_to boards_path
     else
       flash[:danger] = "Username/password incorrect :'("
-      redirect_to '/login'
+      redirect_to login_path
     end
   end
 
   def destroy
     reset_session
     flash[:success] = "Logged out successfully! See you soon."
-    redirect_to '/login'
+    redirect_to login_path
   end
 end
